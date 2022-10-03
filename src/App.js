1c7b3d4116ef9a548843a352a2aa15baca34e224
Oheed911 <oheedimran@outlook.com>
2022-10-03 12:47:53 +0500

    main page component intergrated

--- src/App.js
+++ src/App.js
@@ -4,16 +4,47 @@ import Searchbar from "./components/Searchbar/Searchbar";
 import Card from "./components/Cards/Card"
 import MultiRangeSlider from "./components/Filter/MultiRangeSlider";
 function App() {
+  const data = [
+    {
+      title: "AntDesign",
+      price: "2000",
+      description:"hellooowiow"
+    },
+    {
+      title: "AntDesign",
+      price: "2000",
+      description:"hellooowiow"
+    },
+    {
+      title: "AntDesign",
+      price: "2000",
+      description:"hellooowiow"
+    }
+  ];
   return (
-    <>
-      
-      <MultiRangeSlider
-      min={0}
-      max={1000}
-      onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
-    />
-    </>
-  
+    <div className="App">
+      <div className="searchbar-placement">
+        <Searchbar />
+      </div>
+      <div className="body-placement">
+        <div className="filter-placement">
+
+          <MultiRangeSlider
+            min={0}
+            max={1000}
+            onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
+          />
+        </div>
+        <div className="card-placement">
+          {data.map((data) => (
+            <Card title={data.title} price={data.price} description={data.description}/>
+          ))}
+        </div>
+
+      </div>
+
+    </div>
+
   );
 }
 
--- src/components/Filter/MultiRangeSlider.js
+++ src/components/Filter/MultiRangeSlider.js
@@ -42,40 +42,44 @@ const MultiRangeSlider = ({ min, max, onChange }) => {
   }, [minVal, maxVal, onChange]);
 
   return (
-    <div className="container">
-      <input
-        type="range"
-        min={min}
-        max={max}
-        value={minVal}
-        onChange={(event) => {
-          const value = Math.min(Number(event.target.value), maxVal - 1);
-          setMinVal(value);
-          minValRef.current = value;
-        }}
-        className="thumb thumb--left"
-        style={{ zIndex: minVal > max - 100 && "5" }}
-      />
-      <input
-        type="range"
-        min={min}
-        max={max}
-        value={maxVal}
-        onChange={(event) => {
-          const value = Math.max(Number(event.target.value), minVal + 1);
-          setMaxVal(value);
-          maxValRef.current = value;
-        }}
-        className="thumb thumb--right"
-      />
+    <div className="filter">
+      <p className="Title">Price Filter</p>
+      <div className="container">
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={minVal}
+          onChange={(event) => {
+            const value = Math.min(Number(event.target.value), maxVal - 1);
+            setMinVal(value);
+            minValRef.current = value;
+          }}
+          className="thumb thumb--left"
+          style={{ zIndex: minVal > max - 100 && "5" }}
+        />
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={maxVal}
+          onChange={(event) => {
+            const value = Math.max(Number(event.target.value), minVal + 1);
+            setMaxVal(value);
+            maxValRef.current = value;
+          }}
+          className="thumb thumb--right"
+        />
 
-      <div className="slider">
-        <div className="slider__track" />
-        <div ref={range} className="slider__range" />
-        <div className="slider__left-value">{minVal}</div>
-        <div className="slider__right-value">{maxVal}</div>
+        <div className="slider">
+          <div className="slider__track" />
+          <div ref={range} className="slider__range" />
+          <div className="slider__left-value">{minVal}</div>
+          <div className="slider__right-value">{maxVal}</div>
+        </div>
       </div>
     </div>
+
   );
 };
 
